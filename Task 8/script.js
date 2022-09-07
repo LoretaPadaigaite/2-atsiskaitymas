e2b8c76e6b9fb00447ff7b8dfb8760d7c9c151d3
unknown <loreta.padaigaite@codeacademylt.onmicrosoft.com>
2022-09-07 21:32:28 +0300

    8 task done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class  Calculator {
+    constructor(sum, subtraction, multiplication, division){
+        this.sum = function sum (a, b) {
+            return a + b;    
+        };
+        this.subtraction = function subtraction(a, b) {
+            return a - b;
+        };
+        this.multiplication = function multiplication(a, b) {
+            return a * b;
+        };
+        this.division = function division(a, b) {
+            return a / b;
+        };
+    };
+};
+
+const myCalculator = new Calculator();
+
+console.log(myCalculator.sum(6, 2));
+console.log(myCalculator.subtraction(6, 2));
+console.log(myCalculator.multiplication(6, 2));
+console.log(myCalculator.division(6, 2));
