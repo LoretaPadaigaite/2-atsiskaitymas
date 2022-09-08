2f3e34de9276eadcf2e4cef6f1ec3cc78a635efc
unknown <loreta.padaigaite@codeacademylt.onmicrosoft.com>
2022-09-08 20:43:38 +0300

    5 task done + 6 task half done

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,19 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+//NEVEIKIA
+
+const getUserAverageAge = users.reduce((acc, age) => {
+  return acc + age, 0 / users.length}
+  );
+
+console.log(getUserAverageAge);
+
+
+//antra dalis veikia
+
+const getUsersNames = users.map((users) => {
+  return users.name
+});
+console.log(getUsersNames);
